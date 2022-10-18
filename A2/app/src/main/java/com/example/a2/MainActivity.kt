cde2a8e59ff7ec4aadb3a1c74941900ce22579f8
Michał Dunat <kontaktmichaldunat@gmail.com>
2022-10-18 15:28:49 +0200

    refactor

--- A2/app/src/main/java/com/example/a2/MainActivity.kt
+++ A2/app/src/main/java/com/example/a2/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var initialReceiptValueInput: EditText
     lateinit var maxTipValueInput: EditText
     lateinit var textResult: TextView
-    lateinit var ratingBar : RatingBar
+    lateinit var ratingBar: RatingBar
     var TAG: String = "A2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,21 +22,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun calculateAndSetTotalValue(view: View?) {
-        initialReceiptValueInput = findViewById(R.id.editTextNumberDecimal1)
-        maxTipValueInput = findViewById(R.id.editTextNumberDecimal2)
+        initialReceiptValueInput = findViewById(R.id.initialReceiptValueEditText)
+        maxTipValueInput = findViewById(R.id.maxTipValueEditText)
         textResult = findViewById(R.id.result)
         ratingBar = findViewById(R.id.ratingBar)
-        val editNum1Text = initialReceiptValueInput.text.toString()
-        val editNum2Text = maxTipValueInput.text.toString()
-        if (validateInputFields(editNum1Text, editNum2Text)) return
-        val num1 = editNum1Text.toDouble()
-        val num2 = editNum2Text.toDouble()
+        val initialReceiptValueText = initialReceiptValueInput.text.toString()
+        val maxTipValueText = maxTipValueInput.text.toString()
+        if (validateInputFields(initialReceiptValueText, maxTipValueText)) return
+        val num1 = initialReceiptValueText.toDouble()
+        val num2 = maxTipValueText.toDouble()
         val ratingValue = ratingBar.rating.toDouble() / ratingBar.numStars
         val result: Double = calculateTotalValue(num1, num2, ratingValue)
         displayResult(result)
     }
 
     private fun displayResult(result: Double) {
+        Log.i(TAG, "Displayed value to be paid")
         textResult.text = "Całkowita kwota do zapłaty: " + result.toString() + " zł"
     }
 
@@ -45,19 +46,20 @@ class MainActivity : AppCompatActivity() {
         num2: Double,
         ratingValue: Double
     ): Double {
-        return num1 + (num1 * ((num2 * ratingValue)/100.0))
+        Log.i(TAG, "Calculated total value to be paid")
+        return num1 + (num1 * ((num2 * ratingValue) / 100.0))
     }
 
     private fun validateInputFields(
-        editNum1Text: String,
-        editNum2Text: String
+        initialReceiptValueText: String,
+        maxTipValueText: String
     ): Boolean {
-        if (editNum1Text.isEmpty() || editNum1Text.toDouble() == 0.0) {
+        if (initialReceiptValueText.isEmpty() || initialReceiptValueText.toDouble() == 0.0) {
             Log.e("TAG", "Empty or wrong initial receipt value")
             initialReceiptValueInput.error = "Wprowadź poprawną wartość!"
             return true;
         }
-        if (editNum2Text.isEmpty() || editNum2Text.toDouble() == 0.0) {
+        if (maxTipValueText.isEmpty() || maxTipValueText.toDouble() == 0.0) {
             Log.e("TAG", "Empty or wrong tip value")
             maxTipValueInput.error = "Wprowadź poprawną wartość!"
             return true;
