c90d63e53bd6c6576beb6c8e6011ef3f2bb8fd77
Michał Dunat <kontaktmichaldunat@gmail.com>
2022-10-18 15:10:39 +0200

    transform calculator into tip and receipt value calculating app

--- A2/app/src/main/java/com/example/a2/MainActivity.kt
+++ A2/app/src/main/java/com/example/a2/MainActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.RatingBar
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var editNum1: EditText
-    lateinit var editNum2: EditText
+    lateinit var initialReceiptValueInput: EditText
+    lateinit var maxTipValueInput: EditText
     lateinit var textResult: TextView
+    lateinit var ratingBar : RatingBar
     var TAG: String = "A2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,56 +21,45 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG, "App started running")
     }
 
-    fun calculate(view: View?) {
-        editNum1 = findViewById(R.id.editTextNumberDecimal1)
-        editNum2 = findViewById(R.id.editTextNumberDecimal2)
+    fun calculateAndSetTotalValue(view: View?) {
+        initialReceiptValueInput = findViewById(R.id.editTextNumberDecimal1)
+        maxTipValueInput = findViewById(R.id.editTextNumberDecimal2)
         textResult = findViewById(R.id.result)
-        val editNum1Text = editNum1.text.toString()
-        val editNum2Text = editNum2.text.toString()
+        ratingBar = findViewById(R.id.ratingBar)
+        val editNum1Text = initialReceiptValueInput.text.toString()
+        val editNum2Text = maxTipValueInput.text.toString()
         if (validateInputFields(editNum1Text, editNum2Text)) return
         val num1 = editNum1Text.toDouble()
         val num2 = editNum2Text.toDouble()
-        val result: Double = determineTypeOfCalculationAndPerform(view, num1, num2)
-        textResult.text = java.lang.Double.toString(result)
+        val ratingValue = ratingBar.rating.toDouble() / ratingBar.numStars
+        val result: Double = calculateTotalValue(num1, num2, ratingValue)
+        displayResult(result)
     }
 
-    private fun determineTypeOfCalculationAndPerform(
-        view: View?,
+    private fun displayResult(result: Double) {
+        textResult.text = "Całkowita kwota do zapłaty: " + result.toString() + " zł"
+    }
+
+    private fun calculateTotalValue(
         num1: Double,
-        num2: Double
+        num2: Double,
+        ratingValue: Double
     ): Double {
-        if (view == findViewById(R.id.plusButton)) {
-            Log.i(TAG, "Addition")
-            return num1 + num2
-        } else if (view == findViewById(R.id.minusButton)) {
-            Log.i(TAG, "Subtraction")
-            return num1 - num2
-        } else if (view == findViewById(R.id.multiplyButton)) {
-            Log.i(TAG, "Multiplication")
-            return num1 * num2
-        } else {
-            if (num2 == 0.0) {
-                Log.e("TAG", "Divided by 0")
-                editNum2.error = "Nie można dzielić przez 0!"
-                return Double.NaN;
-            }
-            Log.i(TAG, "Division")
-            return num1 / num2
-        }
+        return num1 + (num1 * ((num2 * ratingValue)/100.0))
     }
 
     private fun validateInputFields(
         editNum1Text: String,
         editNum2Text: String
     ): Boolean {
-        if (editNum1Text.isEmpty()) {
-            Log.e("TAG", "First decimal is empty")
-            editNum1.error = "Wprowadź liczbę!"
+        if (editNum1Text.isEmpty() || editNum1Text.toDouble() == 0.0) {
+            Log.e("TAG", "Empty or wrong initial receipt value")
+            initialReceiptValueInput.error = "Wprowadź poprawną wartość!"
             return true;
         }
-        if (editNum2Text.isEmpty()) {
-            Log.e("TAG", "Second decimal is empty")
-            editNum2.error = "Wprowadź liczbę!"
+        if (editNum2Text.isEmpty() || editNum2Text.toDouble() == 0.0) {
+            Log.e("TAG", "Empty or wrong tip value")
+            maxTipValueInput.error = "Wprowadź poprawną wartość!"
             return true;
         }
         return false
