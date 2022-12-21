781015db89ad0532cfffabc2aa8eb1be6aa35802
Michał Dunat <kontaktmichaldunat@gmail.com>
2022-12-21 13:42:35 +0100

    add not working importing info from contacts functionality

--- BirthdayTracker/app/src/main/java/com/example/birthdaytracker/MainActivity.kt
+++ BirthdayTracker/app/src/main/java/com/example/birthdaytracker/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.birthdaytracker
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.ContactsContract.PhoneLookup
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.room.Room
 import com.example.birthdaytracker.dao.PersonDao
 import com.example.birthdaytracker.database.Database
@@ -26,7 +34,71 @@ class MainActivity : AppCompatActivity() {
 
         personDao = db.personDao()
     }
-    
+
+    fun importContact(view: View) {
+        manageReadContactsPermission()
+        val intent = Intent(Intent.ACTION_PICK).apply {
+            type = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE
+        }
+        importContactActivityLauncher.launch(intent)
+    }
+
+    private fun manageReadContactsPermission() {
+        val permission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_CONTACTS
+        )
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                1
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("TAG", "Permission has been denied by user")
+                } else {
+                    Log.i("TAG", "Permission has been granted by user")
+                }
+            }
+        }
+    }
+
+    private val importContactActivityLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val contactUri: Uri? = it.data?.data
+            val projection: Array<String> = arrayOf(ContactsContract.CommonDataKinds.Phone.NUMBER)
+            if (contactUri != null) {
+                contentResolver.query(contactUri, projection, null, null, null).use { cursor ->
+                    if (cursor != null) {
+                        if (cursor.moveToFirst()) {
+//                            val numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+//                            val number = cursor.getString(numberIndex)
+                            val firstNameIndex =
+                                cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+                            val firstName = cursor.getString(firstNameIndex)
+                            println(firstName)
+//                            println(number)
+                        }
+                    }
+                }
+            }
+        } else {
+        }
+    }
+
     fun runAddPersonActivity(view: View?) {
         val intent = Intent(this, AddPersonActivity::class.java)
         addPersonActivityLauncher.launch(intent)
